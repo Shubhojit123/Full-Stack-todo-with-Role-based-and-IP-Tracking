f24e11d18cc562e81543fdc66d121424517a9507
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2025-07-31 14:51:38 +0530

    Update Notfound.jsx

--- Notfound.jsx
+++ Notfound.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Button, Result } from 'antd';
-const App = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
-  />
-);
+import { useNavigate } from 'react-router-dom';
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const handleBtn = () => {
+    navigate('/');
+  };
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary" onClick={handleBtn}>Back Home</Button>}
+    />
+  );
+};
+
 export default App;
+
